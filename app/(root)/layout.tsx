d61ec13c7f39cba03799590450a719e5e66121d7
Lucas W. Schuch <lucas.schuch@gmail.com>
2025-06-08 11:54:42 -0300

    Refactor code structure for improved readability and maintainability

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <main className="font-work-sans">
+    <main>
       <Navbar />
       {children}
     </main>
